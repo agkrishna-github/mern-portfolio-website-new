7d56f73bff99cc68098d3930337df3b2c8a80383
krishna-ag-github <117165592+krishna-ag-github@users.noreply.github.com>
2024-03-02 13:30:27 +0530

    url updated

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,3 +1,3 @@
 export const base_url =
-  "https://mern-portfolio-website-new-backend.onrender.com";
+  "https://mern-portfolio-website-new-backend.onrender.com/api/";
 // export const base_url = "http://localhost:6060/api/";
